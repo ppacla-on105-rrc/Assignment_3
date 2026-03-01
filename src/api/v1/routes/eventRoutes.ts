1dd524c75685654fb6d3c924bdc160932eca5ab9
Philip Pacla-on <ppacla-on105@academic.rrc.ca>
2026-03-01 13:13:26 -0600

    Added validation

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -1,9 +1,22 @@
 import { Router } from "express";
+import { createEventSchema } from "../validation/eventValidation";
+import { validate } from "../middleware/validate";
 
 const router = Router();
 
+router.post("/", validate(createEventSchema), (req, res) => {
+  res.status(201).json({
+    message: "Event created",
+    data: req.body
+  });
+});
+
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "Events retrieved", count: 0, data: [] });
+  res.status(200).json({
+    message: "Events retrieved",
+    count: 0,
+    data: []
+  });
 });
 
 export default router;
