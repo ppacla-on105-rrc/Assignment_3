c40eecf96d421adc309c55769170e34c5224edb4
Philip Pacla-on <ppacla-on105@academic.rrc.ca>
2026-03-01 12:32:01 -0600

    assignment structure

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import eventRoutes from "./api/v1/routes/eventRoutes";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/v1/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+    version: "1.0.0"
+  });
+});
+
+app.use("/api/v1/events", eventRoutes);
+
+export default app;
